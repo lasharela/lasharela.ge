26b6812b39ad14d62dcce2cae0219043f18dedfd
Lasha Kvantaliani <lasharela@gmail.com>
2018-10-02 14:20:12 +0400

    About content

---
+++ src/config/content.js
@@ -0,0 +1,26 @@
+export const ABOUT = {
+    name: {
+        firstName:'Lasha',
+        lastName:'Kvantaliani',
+    },
+    contact: {
+        address: 'Bay 49th St, Brooklyn, NY 11214',
+        phone: '(317) 585-8468',
+        mail: 'lasharela@gmail.com'
+    },
+    socialNetworks: [
+        {
+            url: 'https://twitter.com/Lasharela',
+            icon: ["fab", "twitter"]
+        },
+        {
+            url: 'https://www.linkedin.com/in/lkvantaliani/',
+            icon: ["fab", "linkedin-in"]
+        },
+        {
+            url: 'https://github.com/lasharela',
+            icon:["fab", "github"]
+        } 
+    ],
+    description: 'I am experienced in leveraging <b>agile frameworks</b> to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.'
+}
